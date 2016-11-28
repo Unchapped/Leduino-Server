38a9f9ec52a28eea673da4e6a4aed518dc69ad82
Unchapped <nate@nathanielchapman.com>
2016-11-27 21:45:37 -0800

    committing my work before I strip out daemonizer in favor of start-stop-daemon

--- configdb.py
+++ configdb.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python3
 from peewee import *
 import os
 
--- leduinod.py
+++ leduinod.py
@@ -1,16 +1,39 @@
+#!/usr/bin/python3
+import os
+import logging
+
 from time import sleep
+
+#this may be redundant, see: http://blog.scphillips.com/posts/2013/07/getting-a-python-script-to-run-in-the-background-as-a-service-on-boot/
 from daemonize import Daemonize
 
 import configdb
+
+#this could be easily made configurable, like in octoprint
+settings_dir = os.path.expanduser("~/.leduino/")
+
+#initialize our settings directory, if it doesn't exist
+try:
+	os.makedirs(settings_dir)
+except OSError as exception:
+	if exception.errno != errno.EEXIST:
+		raise
+
+pid = "/var/run/leduinod.pid"
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
+fh = logging.FileHandler(settings_dir + "leduinod.log", "w")
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+keep_fds = [fh.stream.fileno()]
 
 def main():
 	configdb.open()
 	while True:
 		sleep(5)
-		print('.',end='')
+		logger.debug("I'm alive!")
 	configdb.close()
 
-
-pid = "/tmp/test.pid"
-daemon = Daemonize(app="test_app", pid=pid, action=main)
+daemon = Daemonize(app="leduinod", pid=pid, action=main, keep_fds=keep_fds)
 daemon.start()
